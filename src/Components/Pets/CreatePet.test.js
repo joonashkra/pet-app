83043df61560d52477db4a6a38956b74353552f7
Joonas Heikura <e2101191@edu.vamk.fi>
2024-02-18 00:25:14 +0200

    testcase update

--- src/Components/Pets/CreatePet.test.js
+++ src/Components/Pets/CreatePet.test.js
@@ -2,38 +2,43 @@ import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import CreatePet from './CreatePet';
 import { MemoryRouter } from 'react-router-dom';
-import ListPets from './ListPets';
 
 describe('CreatePet', () => {
   const accessToken = "mockAccessToken";
   const handleOnSubmitMock = jest.fn();
+  const updatePetListMock = jest.fn();
 
   test('creates new pet with valid data', async () => {
-        render(
-        <MemoryRouter>
-            <ListPets accessToken={accessToken} userId={1}/>
-        </MemoryRouter>
-        );
-
-        await waitFor(() => {
-            expect(screen.getByPlaceholderText("Enter pet type...")).toBeInTheDocument();
-            expect(screen.getByPlaceholderText("Enter name...")).toBeInTheDocument();
-            expect(screen.getByLabelText("Date of Birth")).toBeInTheDocument();
-            expect(screen.getByTestId("create-pet-form")).toBeInTheDocument();
-        });
-
-        screen.getByTestId("create-pet-form").onsubmit = handleOnSubmitMock;
-
-        fireEvent.change(screen.getByPlaceholderText('Enter pet type...'), { target: { value: "typeTest" } });
-        fireEvent.change(screen.getByPlaceholderText('Enter name...'), { target: { value: "nameTest" } });
-        fireEvent.change(screen.getByLabelText("Date of Birth"), { target: { value: "2023-12-31" } });
-
-        fireEvent.click(screen.getByTestId("create-pet-btn"));
-
-        await waitFor(() => {
-            expect(handleOnSubmitMock).toHaveBeenCalled();
-        })
+    jest.spyOn(global, "fetch").mockResolvedValueOnce({
+      ok: true,
+      json: () => Promise.resolve({ id: 1 }), // Mocked response for the created pet
     });
+
+    render(
+      <MemoryRouter>
+        <CreatePet userId={1} accessToken={accessToken} updatePetList={updatePetListMock} />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText("Enter pet type...")).toBeInTheDocument();
+      expect(screen.getByPlaceholderText("Enter name...")).toBeInTheDocument();
+      expect(screen.getByLabelText("Date of Birth")).toBeInTheDocument();
+      expect(screen.getByTestId("create-pet-form")).toBeInTheDocument();
+    });
+
+    screen.getByTestId("create-pet-form").onsubmit = handleOnSubmitMock;
+
+    fireEvent.change(screen.getByPlaceholderText('Enter pet type...'), { target: { value: "typeTest" } });
+    fireEvent.change(screen.getByPlaceholderText('Enter name...'), { target: { value: "nameTest" } });
+    fireEvent.change(screen.getByLabelText("Date of Birth"), { target: { value: "2023-12-31" } });
+
+    fireEvent.click(screen.getByTestId("create-pet-btn"));
+
+    await waitFor(() => {
+      expect(handleOnSubmitMock).toHaveBeenCalled();
+    });
+  });
 
   test('shows error on invalid data', async () => {
     render(
